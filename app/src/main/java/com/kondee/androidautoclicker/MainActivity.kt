58c563295ec6136acec5f35759bcdc6e095fb5fa
Karunrat Khakhom <k.karunrat@gmail.com>
2019-06-07 17:59:43 +0700

    Update MenuService

--- app/src/main/java/com/kondee/androidautoclicker/MainActivity.kt
+++ app/src/main/java/com/kondee/androidautoclicker/MainActivity.kt
@@ -1,12 +1,48 @@
 package com.kondee.androidautoclicker
 
+import android.annotation.TargetApi
+import android.content.Intent
+import android.net.Uri
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import com.kondee.androidautoclicker.services.MenuService
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    private var serviceIntent: Intent? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initInstance()
+    }
+
+    private fun initInstance() {
+        button.setOnClickListener {
+            if (Build.VERSION.SDK_INT < Build.VERSION_CODES.N
+                    || Settings.canDrawOverlays(this)) {
+                serviceIntent = Intent(this@MainActivity,
+                        MenuService::class.java)
+                startService(serviceIntent)
+                onBackPressed()
+            } else {
+                askPermission()
+            }
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.M)
+    private fun askPermission() {
+        val intent = Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION,
+                Uri.parse("package:$packageName"))
+        startActivityForResult(intent, PERMISSION_REQUEST_CODE)
+    }
+
+    companion object {
+        private const val PERMISSION_REQUEST_CODE = 123
     }
 }
--- app/src/main/java/com/kondee/androidautoclicker/services/MenuService.kt
+++ app/src/main/java/com/kondee/androidautoclicker/services/MenuService.kt
@@ -3,14 +3,21 @@ package com.kondee.androidautoclicker.services
 import android.app.Service
 import android.content.Context
 import android.content.Intent
+import android.graphics.PixelFormat
+import android.os.Build
 import android.os.IBinder
 import android.view.LayoutInflater
+import android.view.View
 import android.view.WindowManager
+import android.widget.ImageButton
 import com.kondee.androidautoclicker.R
 
 class MenuService : Service() {
 
     private var windowManager: WindowManager? = null
+    private var layoutParams: WindowManager.LayoutParams? = null
+
+    private var view: View? = null
 
     override fun onBind(intent: Intent?): IBinder? {
         return null
@@ -21,6 +28,31 @@ class MenuService : Service() {
 
         view = LayoutInflater.from(this).inflate(R.layout.layout_floating_menu, null)
 
+        val buttonPlayPause: ImageButton? = view?.findViewById(R.id.button_play_pause)
+        val buttonAdd: ImageButton? = view?.findViewById(R.id.button_add)
+        val buttonClose: ImageButton? = view?.findViewById(R.id.button_close)
+
+        val overlayParam = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            WindowManager.LayoutParams.TYPE_APPLICATION_OVERLAY
+        } else {
+            WindowManager.LayoutParams.TYPE_PHONE
+        }
+
+        layoutParams = WindowManager.LayoutParams(
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                WindowManager.LayoutParams.WRAP_CONTENT,
+                overlayParam,
+                WindowManager.LayoutParams.FLAG_NOT_FOCUSABLE,
+                PixelFormat.TRANSLUCENT)
+
         windowManager = getSystemService(Context.WINDOW_SERVICE) as WindowManager
+        windowManager?.addView(view, layoutParams)
+
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        windowManager?.removeView(view)
     }
 }
